24c7b5f01c43bc2f7c7fed891cbc29e0639618f6
Nguyen Van Anh Tuan <92255316+navtuan12@users.noreply.github.com>
2024-12-26 13:31:46 +0700

    Update index.js

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8800/api-v1";
+const API_URL = "http://api.devnoneknow.online/api-v1";
 
 export const API = axios.create({
   baseURL: API_URL,
